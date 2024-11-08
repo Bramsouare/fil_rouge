9fe748557b499ac42ea548eb365875b153dc9f0a
souare ibrahima <souarei404@gmail.com>
2024-11-08 10:39:19 +0100

    village_green

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,11 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
-    #[Route('/accueil', name: 'app_accueil')]
+    #[Route(
+        '/accueil',
+        name: 'app_accueil'
+        )
+    ]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig', [
+        return $this -> render
+        (
+            'accueil/index.html.twig',
+            [
             'controller_name' => 'AccueilController',
-        ]);
+            ]
+        );
     }
 }
