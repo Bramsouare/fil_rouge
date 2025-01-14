9e014906a8dff7922304dd448c1ed4cf6ac52d73
souare ibrahima <souarei404@gmail.com>
2025-01-14 16:54:30 +0100

    rubrique

--- src/Controller/CatalogueController.php
+++ src/Controller/CatalogueController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Rubrique;
+use App\Repository\ProduitRepository;
+use App\Repository\RubriqueRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CatalogueController extends AbstractController
 {
@@ -15,6 +18,8 @@ class CatalogueController extends AbstractController
             'controller_name' => 'RechercheController',
         ]);
     }
+
+
     /*###################################################################################################################################
         *                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    ACCUEIL CONTROLLER    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ###################################################################################################################################*/
@@ -24,17 +29,52 @@ class CatalogueController extends AbstractController
         name: 'app_accueil'
         )
     ]
-    public function accueil(): Response
+    public function accueil(RubriqueRepository $repository, ProduitRepository $produitRepository): Response
     {
+        $rubriques = $repository->findAll();
+        // dd($rubriques);
+        
+        $produits = $produitRepository->findby(['produit_stock' => 5], null, 3);
         return $this -> render
             (
                 'catalogue/accueil.html.twig',
                 [
                     'controller_name' => 'CatalogueController',
+                    'rubriques' => $rubriques,
+                    'produits' => $produits
+
                 ]
             )
         ;
     }
+
+
+    /*####################################################################################################################################
+    *                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~      RUBRIQUES CONTROLLER     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ####################################################################################################################################*/
+
+    #[Route(
+        '/rubrique/{id}',
+        name: 'app_rubrique_produits'
+            )
+        ]
+    public function produitRubrique(Rubrique $rubrique): Response
+    {
+        $produits = $rubrique->getProduit();
+        //dd($produits);
+
+        return $this -> render
+            (
+                'catalogue/produitsRubrique.html.twig',
+                [
+                    'controller_name' => 'CatalogueController',
+                    'rubrique' => $rubrique,
+                    'produits' => $produits
+                ]
+            )
+        ;
+    }
+
 
     /*####################################################################################################################################
     *                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~      CATEGORIES CONTROLLER     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -45,19 +85,19 @@ class CatalogueController extends AbstractController
         name: 'app_categorie'
             )
         ]
-    public function categorie(): Response
+    public function categorie(RubriqueRepository $repository): Response
     {
         return $this -> render
             (
                 'catalogue/categorie.html.twig',
                 [
                     'controller_name' => 'CatalogueController',
+                    'rubriques' => $repository->findAll()
                 ]
             )
         ;
     }
 
-    
 
     /*####################################################################################################################################
     *                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~      INSTRUMENT CONTROLLER     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -68,11 +108,82 @@ class CatalogueController extends AbstractController
         name: 'app_instrument'
             )
         ]
-    public function instrument(): Response
+    public function instrument(ProduitRepository $repository): Response
     {
+        $produits = $repository->findAll();
+        //dd($produits);
+
         return $this -> render
             (
                 'catalogue/instrument.html.twig',
+                [
+                    'controller_name' => 'CatalogueController',
+                    'produits' => $produits
+                ]
+            )
+        ;
+    }
+
+
+    /*####################################################################################################################################
+    *                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~      CGU CONTROLLER     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ####################################################################################################################################*/
+
+    #[Route(
+        '/cgu',
+        name: 'app_cgu'
+            )
+        ]
+    public function cgu(): Response
+    {
+       
+        return $this -> render
+            (
+                'catalogue/cgu.html.twig',
+                [
+                    'controller_name' => 'CatalogueController',
+                ]
+            )
+        ;
+    }
+
+    /*####################################################################################################################################
+    *           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~      POLITIQUE DE CONFIDENTIALITÉ CONTROLLER     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ####################################################################################################################################*/
+
+    #[Route(
+        '/politiqueConf',
+        name: 'app_politiqueConf'
+            )
+        ]
+    public function politiqueConf(): Response
+    {
+        
+        return $this -> render
+            (
+                'catalogue/politiqueConf.html.twig',
+                [
+                    'controller_name' => 'CatalogueController',
+                ]
+            )
+        ;
+    }
+
+    /*####################################################################################################################################
+    *                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~      MENTIONS LÉGALES CONTROLLER     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ####################################################################################################################################*/
+
+    #[Route(
+        '/mentionsLegales',
+        name: 'app_mentionsLegales'
+            )
+        ]
+    public function mentionsLegales(): Response
+    {
+       
+        return $this -> render
+            (
+                'catalogue/mentionsLegales.html.twig',
                 [
                     'controller_name' => 'CatalogueController',
                 ]
